02499da061ae646f4850f2cfe080cc7b2462b474
MOhhh <64732359+MOhhh-ok@users.noreply.github.com>
2024-12-27 18:53:32 +0900

    -

--- web-file-kit/src/converts.ts
+++ web-file-kit/src/converts.ts
@@ -1,4 +1,4 @@
-export async function fileToBase64(file: File): Promise<string> {
+export async function fileToBase64(file: File | Blob): Promise<string> {
     return new Promise((resolve, reject) => {
         const reader = new FileReader();
         reader.onloadend = () => resolve(reader.result as string);
